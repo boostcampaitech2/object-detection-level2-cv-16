ff59a8adeb5c056eedec695f03306b2c49ab043e
SEUNGWOO LEE <69357689+DaleLeeCoding@users.noreply.github.com>
2021-10-03 21:54:07 +0900

    Add 35-SwinTransformer-B

---
+++ mmdetection/configs/35-swin/default_runtime.py
@@ -0,0 +1,23 @@
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+        dict(
+        type='WandbLoggerHook',
+        init_kwargs=dict(
+            project='mmdetection',
+            name='35-SwinTransformer',
+            entity = 'passion-ate'
+        ))
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
