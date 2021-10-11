ac7484fe05f7f73bda119d904f0b424f5882ead6
Haeun Oh <69044270+Haeun-Oh@users.noreply.github.com>
2021-10-12 08:57:20 +0900

    Fix yolor_make_submission.py to adjust size

--- yolor/yolor_make_submission.py
+++ yolor/yolor_make_submission.py
@@ -2,40 +2,50 @@ import os
 import copy
 import pandas as pd
 
-DETECT_TXT_DIR = 'yolor/inference/yolor_p67_best'
-IMAGE_DIR = 'yolor/trash_data/images/test'
-SUBMISSION_NAME = './yolor_p67_best_submission_2.csv'
+def adjust_size(tmp):
+    if int(tmp) == 0:
+        tmp = 0
+    elif int(tmp) == 1024:
+        tmp = 1024
+    return tmp
 
-whole = sorted(os.listdir(IMAGE_DIR))
-label_name = [x[:-3]+'txt' for x in whole]
+def main():
+    DETECT_TXT_DIR = 'yolor_paper/yolor/runs/yolor-e6/exp/labels'
+    IMAGE_DIR = 'yolor_paper/yolor/trash_data/images/test'
+    SUBMISSION_NAME = './yolor_e6_best_submission_resize.csv'
 
-file_names = []
-prediction_strings = []
+    whole = sorted(os.listdir(IMAGE_DIR))
+    label_name = [x[:-3]+'txt' for x in whole]
 
-for i in range(len(whole)):
-    file = DETECT_TXT_DIR + '/' + label_name[i]
-    image_id = whole[i][:4] + '/' + whole[i][5:]
-    if os.path.isfile(file):
-        f = open(file)
-        line = f.readline()
-        predict = ''
-        while line:
-            line = map(float, line.rstrip().split())
-            label, score, xc, yc, w, h  = line
-            w *= 1024
-            h *= 1024
-            x = xc * 1024 - w / 2
-            y = yc * 1024 - h / 2
-            predict += str(int(label)) + ' ' + str(score) + ' ' + str(x) + ' ' + str(
-                    y) + ' ' + str(x + w) + ' ' + str(y + h) + ' '
+    file_names = []
+    prediction_strings = []
+
+    for i in range(len(whole)):
+        file = DETECT_TXT_DIR + '/' + label_name[i]
+        image_id = whole[i][:4] + '/' + whole[i][5:]
+        if os.path.isfile(file):
+            f = open(file)
             line = f.readline()
-    else:
-        predict = ''
-    file_names.append(image_id)
-    prediction_strings.append(predict)
+            predict = ''
+            while line:
+                line = map(float, line.rstrip().split())
+                label, xc, yc, w, h, score  = line
+                w *= 1024
+                h *= 1024
+                x = xc * 1024 - w / 2
+                y = yc * 1024 - h / 2
+                predict += str(int(label)) + ' ' + str(score) + ' ' + str(adjust_size(x)) + ' ' + str(adjust_size(y)) + ' ' + str(adjust_size(x + w)) + ' ' + str(adjust_size(y + h)) + ' '
+                line = f.readline()
+        else:
+            predict = ''
+        file_names.append(image_id)
+        prediction_strings.append(predict)
 
-submission = pd.DataFrame()
-submission['PredictionString'] = prediction_strings
-submission['image_id'] = file_names
-submission.to_csv(SUBMISSION_NAME, index=None)
-print(submission.head())
+    submission = pd.DataFrame()
+    submission['PredictionString'] = prediction_strings
+    submission['image_id'] = file_names
+    submission.to_csv(SUBMISSION_NAME, index=None)
+    print(submission.head())
+    
+if __name__ == '__main__':
+    main()
